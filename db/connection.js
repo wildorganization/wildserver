9e5b89df1a692b3facb72d2d418ef60e0e093a99
Filipe Marques Moreira <filipemoreira23@gmail.com>
2017-03-19 01:55:55 -0300

    Server now handles all queries in one place and supports a pool with multiple connections

--- db/connection.js
+++ db/connection.js
@@ -1,28 +1,57 @@
 var mysql = require('mysql');
 
 module.exports = {
-    getConnection: getConnection
+    executeQuery: executeQuery
 }
 
-function getConnection() {
-	// Instanciando conexao
-	var connection = mysql.createConnection({
-	  host     : 'localhost',
-	  user     : 'root',
-	  password : 'cenoura',
-	  database : 'bd_findup'
-	});
+// Instanciando conexao
+var pool = mysql.createPool({
+	connectionLimit: 100,
+	host     : 'localhost',
+	user     : 'root',
+	password : 'cenoura',
+	database : 'bd_findup',
+	debug	: false
+});
 
-	// tentando conectar com o banco
-	connection.connect(function(err){
-		if(!err) {
-		    console.log("Database is connected ... nn");    
-		    return connection;
-		} else {
-			return -1;
-		    console.log("Error connecting database ... nn");    
-		}
-	});
+function executeQuery(req,res,query) {
+	pool.getConnection(function(err,connection){
+        if (err) {
+			res.status(100).json({
+				status: 'error', 
+				data: err,
+				message: "Error in connection database"
+			});
+			return;
+        }   
 
-	return connection;
+        console.log('connected as id ' + connection.threadId);
+        
+        connection.query(query,function(err,rows){
+            connection.release();    
+            if(!err) {
+                res.status(200).json({
+                	status: 'success',
+                	data: rows,
+                	message: 'Query performed successfuly'
+                });
+            } else {
+            	console.log('Error while performing Query.');
+	            res.status(500).json({
+	                status: 'error',
+	                data: err,
+	                message: 'Error while performing Query.'
+	            });
+            }  
+        });
+
+        connection.on('error', function(err) {      
+            res.status(100).json({
+            	status: 'error', 
+            	data: err,
+            	message: "Error in connection database"
+            });
+            return;     
+        });
+	});
 }
--- db/users.js
+++ db/users.js
@@ -13,30 +13,5 @@ module.exports = {
 var connection;
 
 function getAllUsers(req, res, next) {
-    connection = db.getConnection();
-    
-    if (connection === -1) {
-        console.log("Conexion failed.");
-        return {error: "Conexion failed."}
-    };
-
-    connection.query('SELECT * FROM tb_usuarios', function(err, rows, fields) {
-        connection.end();
-        if (!err) {
-            console.log('Data: ', rows);
-            res.status(200).json({
-                status: 'success',
-                data: rows,
-                message: 'Retrieved ALL users'
-            });
-        } else {
-            console.log('Error while performing Query.');
-            res.status(500).json({
-                status: 'error',
-                data: err,
-                message: 'Error while performing Query.'
-            });
-        }
-    });
-
+    db.executeQuery(req,res,'SELECT * FROM tb_usuarios');
 }
